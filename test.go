a6a4e64566335155285a9ece3a780457bbcfbad0
Anita Amini <anita.amini@cohaesus.co.uk>
2016-08-22 21:02:21 +0100

    first commit

--- test.go
+++ test.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("Enter text: ")
+    name, _ := reader.ReadString('\n')
+    fmt.Scanf(name)
 }
+
+//func ask(string name, int age, string favLang){
+
+//}
+
