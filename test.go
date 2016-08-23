e0a2a06e9813ad8c879a7bf5e2c6821b7fad80c3
Neats29 <baadge29@yahoo.co.uk>
2016-08-23 17:20:41 +0100

    added age

--- test.go
+++ test.go
@@ -8,12 +8,15 @@ import (
 
 func main() {
     reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Enter text: ")
+    fmt.Print("What is your age: ")
+    //TODO: check for age to be no more than 3 digits
+    age, _ := reader.ReadString('\n')
+    fmt.Scanf(age)
+    fmt.Print("What is your name: ")
     name, _ := reader.ReadString('\n')
     fmt.Scanf(name)
+    fmt.Print("Your name is ",  name, " and you are ", age, " years old")
 }
 
-//func ask(string name, int age, string favLang){
 
-//}
 
